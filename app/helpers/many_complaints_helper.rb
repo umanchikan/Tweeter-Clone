c9a9fe883452e267ea710c1b5b8e4b8acd3ffd1c
chiakisaito <a.cop.p2p2@gmail.com>
2020-05-20 23:39:00 +0900

    登録内容確認→戻る→再度登録の際に確認画面を出す

--- app/helpers/many_complaints_helper.rb
+++ app/helpers/many_complaints_helper.rb
@@ -1,9 +1,9 @@
 module ManyComplaintsHelper
   def choose_new_or_edit
-    if action_name == 'new'
+    if action_name == 'new'|| action_name == 'create'
       confirm_many_complaints_path
     elsif action_name == 'edit'
-      many_complaints_path
+      many_complaint_path
     end
   end
 end

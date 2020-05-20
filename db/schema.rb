f725cbf33ac2b61d794eadd2f0f43351d681b930
chiakisaito <a.cop.p2p2@gmail.com>
2020-05-20 23:51:53 +0900

    gemfile,.gitignoreの不要なコメントを消去

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2020_05_16_081604) do
 
-  # These are extensions that must be enabled in order to support this database
+
   enable_extension "plpgsql"
 
   create_table "many_complaints", force: :cascade do |t|
